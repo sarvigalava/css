7852d4fbcb82a39eae2010616f70d30a6a84e835
sarvigalava <sarvigalava@gmail.com>
2015-05-11 20:35:00 +0300

    added candidates table on ui

---
+++ main/src/ro/info/uaic/persistence/PersistenceService.java
@@ -0,0 +1,39 @@
+package ro.info.uaic.persistence;
+
+import org.springframework.stereotype.Service;
+import ro.info.uaic.models.Candidate;
+import ro.info.uaic.models.TestType;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by lotus on 11.05.2015.
+ */
+@Service
+public class PersistenceService
+{
+    public List<Candidate> getAllCandidates()
+    {
+        List<Candidate> candidates = new ArrayList<>();
+        for (int i=0; i<100; ++i)
+        {
+            Candidate candidate = new Candidate();
+            candidate.setAddress("Address " + i);
+            candidate.setFirstName("First Name " + i);
+            candidate.setLastName("Last Name " + i);
+            candidate.setCnp("CNP " + i);
+            candidate.setBirthDate(new Date());
+            candidate.setTestType(TestType.NONE);
+            candidate.setTestMark(i);
+            candidate.setAvgBac(i);
+            candidate.setBacDomain(TestType.MATH);
+            candidate.setBacDomainMark(i);
+            candidate.setApplicationDate(new Date());
+            candidates.add(candidate);
+        }
+
+        return candidates;
+    }
+}
--- main/src/ro/info/uaic/ui/IndexBean.java
+++ main/src/ro/info/uaic/ui/IndexBean.java
@@ -1,30 +1,34 @@
 package ro.info.uaic.ui;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import ro.info.uaic.models.Candidate;
+import ro.info.uaic.persistence.PersistenceService;
 
+import javax.annotation.PostConstruct;
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * Created by lotus on 03.05.2015.
  */
 @Component
-@Scope("request")
+@Scope("session")
 public class IndexBean implements Serializable
 {
-    private boolean selected = true;
-    public boolean isSelected() {
-        return selected;
+    @Autowired PersistenceService persistenceService;
+
+    private List<Candidate> candidates;
+
+    @PostConstruct
+    public void init()
+    {
+        candidates = persistenceService.getAllCandidates();
     }
 
-    public void setSelected(boolean selected) {
-        this.selected = selected;
-    }
-
-    public String getBoxValueDescription() {
-        if(selected)
-            return "selected";
-        else
-            return "unselected";
+    public List<Candidate> getCandidates()
+    {
+        return candidates;
     }
 }
